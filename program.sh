66968913d3cb86b6d750ad49d0193d3cdc0a125a
Zicklag <zicklag@katharostech.com>
2020-05-19 23:46:46 +0000

    Add Pizza Check

--- program.sh
+++ program.sh
@@ -5,3 +5,8 @@ read -p 'What is your favorite food: ' food
 
 echo "Hi $name! My name is $0."
 echo "I like $food, too!"
+
+if [ "$food" == "pizza" ]; then
+    echo "But really, everybody likes pizza. ;)"
+fi
+
