be292aa967cce8fa624cc0fc41589d5565f7e2bc
Zicklag <zicklag@katharostech.com>
2020-05-19 23:51:17 +0000

    Add Cats VS Dogs

--- program.sh
+++ program.sh
@@ -10,3 +10,14 @@ if [ "$food" == "pizza" ]; then
     echo "But really, everybody likes pizza. ;)"
 fi
 
+echo "Oh, no a bug!!!!"
+
+read -p "OK, next quesion, cats or dogs?: " pet
+
+if [ "$pet" == "cats" ]; then
+    echo "Yep, cat's are totally better"
+elif [ "$pet" == "dogs" ]; then
+    echo "Eh, I'm more of a cat person really"
+else
+    echo "Well, that wasn't really an option, but I gess $pet are OK, too."
+fi
