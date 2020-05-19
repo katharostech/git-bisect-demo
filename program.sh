55350cc7d65654586e9ab5d658b8624b9af216fd
Zicklag <zicklag@katharostech.com>
2020-05-19 23:45:04 +0000

    Add Food Question

--- program.sh
+++ program.sh
@@ -1,5 +1,7 @@
 echo "Hello! Let's find out more about you."
 
 read -p 'What is your name: ' name
+read -p 'What is your favorite food: ' food
 
 echo "Hi $name! My name is $0."
+echo "I like $food, too!"
